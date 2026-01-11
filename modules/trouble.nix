b4eb4806843380b403c9e20d71c71d067d7c4693
TypicalUsername-ai <mati.domalewski@gmail.com>
2026-01-11 21:58:55 +0000

    trouble: add extra config and force non-lazy loading

--- modules/trouble.nix
+++ modules/trouble.nix
@@ -1,11 +1,33 @@
-{ config, pkgs, ... }:
 {
-  config.vim.lsp.trouble = {
-    enable = true;
-    mappings = {
-      symbols = "<leader>ts";
-      quickfix = "<leader>qf";
-      workspaceDiagnostics = "<leader>tt";
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  config.vim = {
+    lsp.trouble = {
+      enable = true;
+      mappings = {
+        symbols = "<leader>ts";
+        quickfix = "<leader>qf";
+        workspaceDiagnostics = "<leader>ld";
+      };
     };
+
+    keymaps = lib.mkAfter [
+      {
+        key = "<leader>xx";
+        mode = [
+          "n"
+          "v"
+        ];
+        action = "<cmd>Trouble diagnostics toggle<CR>";
+        silent = true;
+        desc = "Diagnostics (Trouble)";
+      }
+    ];
+
+    lazy.plugins.trouble.lazy = lib.mkForce false;
   };
 }
